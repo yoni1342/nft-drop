a85ab36c3941f8f1f4207b7770f1e3badc80ad3d
yoni <yoni@kali>
2022-04-01 18:30:29 +0300

    Day 2 work

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,17 @@ import Head from 'next/head'
 import {MoonIcon, SunIcon} from '@heroicons/react/outline'
 import { useEffect, useState } from 'react';
 import Link from 'next/link'
+import { GetServerSideProps } from 'next';
+import {sanityClient} from '../lib/sanity.server'
+import {urlFor} from '../lib/sanity'
+import { Collection } from '../typings';
 
+interface Props{
+  collections: Collection[]
+}
 
-export default function NextPage(){
+export default function NextPage({collections}:Props){
+  console.log(collections)
   const {systemTheme, theme, setTheme} = useTheme();
   const [mounted, setMounted] = useState(false);
   useEffect(()=>{
@@ -26,7 +34,7 @@ export default function NextPage(){
     }
 }
   return (
-    <div className="">
+    <div className="snap-y snap-mandatory overflow-scroll overflow-x-hidden">
       <Head>
         <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
@@ -35,7 +43,7 @@ export default function NextPage(){
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&display=swap" rel="stylesheet"/>
       </Head>
 
-      <div className = 'h-screen w-screen  flex flex-col transition duration-300 ease-in-out  dark:transition dark:duration-300 dark:ease-in-out dark:bg-gray-900' >
+      <div className = 'snap-start h-screen w-screen  flex flex-col transition duration-300 ease-in-out  dark:transition dark:duration-300 dark:ease-in-out dark:bg-gray-900 ' >
         <nav className ='flex justify-between px-10 py-5'>
           <h1 className = 'font-jose text-2xl cursor-pointer'>Yoni APE Clube</h1>
           <div>
@@ -45,10 +53,10 @@ export default function NextPage(){
         <div className = 'w-screen flex justify-center items-center h-screen'>
           <div className = 'md:flex md:items-center md:justify-center text-center md:space-x-5 lg:w-[1400px] space-y-20 p-10'>
             <div className ='p-2 text-gray-800 '>
-              <h1 className = 'font-bold font-jose text-3xl p-3 dark:text-gray-200'>Welcome to the yoni ape clube</h1>
-              <p className ='dark:text-white text-lg'>The Ape Clube Collection is a collection of 10000 APE CLUBE. generation randomly from over 100+ assets. Each APE CLUBE is a unique creation with different traits. Ranging form accessories to facial hair. no two are the same. All live happily on the Ethereum Blockchain</p>
-              <Link href = '/nft/yoniape'>
-               <button className = 'bg-rose-400 px-4 py-2 mt-2 rounded-full text-white font-bold hover:shadow-md hover:scale-105 active:scale-95 active:bg-rose-600 transition saturate-150 ease-in-out '>Go To Yoni Ape</button>
+              <h1 className = 'font-bold font-jose text-3xl p-3 dark:text-gray-200'>Welcome to the yoni NFT Collection</h1>
+              <p className ='dark:text-white text-lg'>The Yoni NFT Collection has a collection of 10000 NFT arts. generation randomly from over 100+ assets. Each NFT is a unique creation with different traits. Ranging form accessories to facial hair. no two are the same. All live happily on the Ethereum Blockchain</p>
+              <Link href = '#collections'>
+               <button className = 'bg-rose-400 px-4 py-2 mt-2 rounded-full text-white font-bold hover:shadow-md hover:scale-105 active:scale-95 active:bg-rose-600 transition saturate-150 ease-in-out '>Go To Collections</button>
               </Link>
             </div>
             <div>
@@ -58,8 +66,59 @@ export default function NextPage(){
         </div>
       </div>
 
+      <div className = ' snap-start dark:bg-gray-900 h-screen flex justify-center items-center px-10 mt-20 md:mt-0' id='collections'>
+        <div className = 'dark:bg-gradient-to-br dark:from-white/20  w-screen dark:to-gray-900 bg-opacity-10 rounded-3xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-10 bg-gradient-to-br from-gray-300/50 to-gray-50/50 shadow-sm space-y-10 md:space-y-0 items-center'>
+
+          {collections.map(collection =>(
+            <Link href = {`/nft/${collection.slug.current}`}>
+              <div className ='flex cursor-pointer flex-col items-center hover:scale-105 transition-all duration-200'>
+              {console.log(collection )}
+                <img src={urlFor(collection.mainImage).url()} alt="" className ='h-96 w-60 rounded-2xl object-cover' />
+                <div>
+                  <h2 className = 'text-2xl md:text-3xl'>{collection.title}</h2>
+                  <p className = 'mt-2 text-sm font-light text-gray-600'>{collection.description}</p>
+                </div>
+              </div>
+            </Link>
+            
+
+          ))}
+        </div>
+      </div>
+
     </div>
   )
 }
 
 
+export const getServerSideProps: GetServerSideProps = async()=>{
+    const query = `  *[_type == "collection"]{
+      _id,
+      title,
+      address,
+      description,
+      nftCollectionName,
+      mainImage{
+      asset
+    },
+    slug {
+      current
+    },
+    creator->{
+      _id,
+      name,
+      address,
+      slug{
+      current
+    },
+    },
+    }`
+
+    const collections = await sanityClient.fetch(query);
+    console.log(collections)
+  return {
+    props:{
+      collections
+    }
+  }  
+}
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -4,8 +4,18 @@ import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 import {useTheme} from 'next-themes';
 import {MoonIcon, SunIcon} from '@heroicons/react/outline'
 import Link from 'next/link';
+import { GetServerSideProps } from 'next';
+import { sanityClient } from '../../lib/sanity.server';
+import { id } from '@ethersproject/hash';
+import {urlFor} from '../../lib/sanity'
+import { Collection } from '../../typings';
 
-function NFTDropPage() {
+interface Props{
+    collection: Collection
+}
+
+function NFTDropPage({collection}:Props) {
+    console.log(collection);    
     const {systemTheme, theme, setTheme} = useTheme();
     const [mounted, setMounted] = useState(false);
     useEffect(()=>{
@@ -37,11 +47,11 @@ function NFTDropPage() {
         <div className = 'bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4 dark:bg-gradient-to-br dark:from-cyan-900 dark:to-rose-900 transition duration-700 ease-in-out lg:h-screen'>
         <div className ='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
             <div className ='bg-gradient-to-br from-yellow-400 to-purple-600 p-1 rounded-xl dark:bg-gradient-to-br dark:from-yellow-700 dark:to-green-400' >
-                <img src="https://links.papareact.com/8sg" alt=""  className ='w-44 rounded-xl object-cover lg:h-96 lg:w-72'/>
+                <img src={urlFor(collection.previewImage).url()} alt=""  className ='w-44 rounded-xl object-cover lg:h-96 lg:w-72'/>
             </div>
         <div className = 'space-y-2 p-5 text-center'>
-            <h1 className ='text-4xl font-bold text-white dark:text-gray-200' >Yoni Apes</h1>
-            <h2 className = 'text-xl text-gray-300'>A collection of yoni Apes who live & breathe React with papafam 🥳 ! </h2>
+            <h1 className ='text-4xl font-bold text-white dark:text-gray-200' >{collection.nftCollectionName}</h1>
+            <h2 className = 'text-xl text-gray-300'>{collection.description} 🥳 ! </h2>
         </div>
         </div>
         </div>
@@ -50,7 +60,7 @@ function NFTDropPage() {
             {/* Header */}
         <header className = 'flex items-center justify-between'>
             <h1 className = 'cursor-pointer w-52 text-xl font-extralight sm:w-80'>
-                The <Link href = '/'><span className ='font-extrabold underline decoration-pink-600/50 hover:bg-gray-800 hover:p-2 rounded-full'> YONI</span></Link>  NFT Market Place
+                The <Link href = '/'><span className ='font-extrabold underline decoration-pink-600/50 hover:bg-slate-200 dark:hover:bg-gray-800 hover:p-2 rounded-full'> YONI</span></Link>  NFT Market Place
             </h1>
             <div className = 'flex items-center space-x-5'>
                 <button className ='rounded-full bg-rose-400 dark:bg-rose-700 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base hover:shadow-lg dark:hover:shadow-gray-700 hover:scale-105 active:scale-95 active:bg-rose-500 dark:active:bg-rose-800 transtion duration-150 ease-in-out' onClick = {()=>(address? disconnect(): connectWithMetamask())}>{address ? 'Sign Out': 'Sign In'}</button>
@@ -63,9 +73,9 @@ function NFTDropPage() {
         )}
             {/* Contente */}
         <div className = 'mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0 '>
-            <img src="https://links.papareact.com/bdy" alt="" className = 'w-80 object-cover pb-10 lg:h-40 ' />
+            <img src={urlFor(collection.mainImage).url()} alt="" className = 'w-80 object-cover pb-10 lg:h-40 ' />
             <h1 className = 'text-3xl font-bold lg:text-5xl lg:font-extrabold'>
-                The PAPAFAM Ape Coding Clube | NFT Drop
+                {collection.title}
             </h1>
             <p className ='pt-2 text-xl text-green-500'>13 /21 NFT's Claimed</p>
         </div>
@@ -77,3 +87,51 @@ function NFTDropPage() {
 }
 
 export default NFTDropPage;
+
+
+
+export const getServerSideProps: GetServerSideProps = async ({params})=>{
+    const query = `*[_type == "collection" && slug.current == $id][0]{
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage{
+            asset
+        },
+        previewImage{
+            asset
+        },
+        slug{
+            current
+        },
+        creator->{
+            _id,
+            name,
+            address,
+            slug{
+                current
+            },
+        },
+    }
+    
+    `
+    const collection = await sanityClient.fetch(query, {
+        id: params?.id
+    })
+
+    if(!collection){
+        return{
+            notFound: true
+        }
+    }
+
+    return{
+        props: {
+            collection
+        }
+    }
+
+
+}
